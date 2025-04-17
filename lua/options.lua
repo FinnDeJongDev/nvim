c9cecc35a10cb36e5f53d92eb393392454bc7586
FinnDeJongDev <finn.dejong.dev@gmail.com>
2025-04-17 16:23:45 +0200

    small changes

--- lua/options.lua
+++ lua/options.lua
@@ -6,7 +6,7 @@ vim.opt.mouse = 'a'
 vim.opt.tabstop = 2
 vim.opt.softtabstop = 2
 vim.opt.shiftwidth = 2
-vim.opt.expandtab = true
+vim.opt.expandtab = false
 
 vim.opt.undofile = true
 
@@ -16,14 +16,17 @@ vim.opt.wrap = false
 
 vim.opt.signcolumn = 'yes'
 
-vim.opt.updatetime = 250
+vim.opt.updatetime = 50
 
 vim.opt.splitright = true
 vim.opt.splitbelow = false
 
 vim.opt.cursorline = true
 
-vim.opt.scrolloff = 8
+vim.opt.scrolloff = 10
+
+vim.opt.list = true
+vim.opt.listchars = { tab = "|\\ " }
 
 -- Yank highlight
 vim.api.nvim_create_autocmd('TextYankPost', {
